ec2ca3f460163090b8a492ed0e1e6a0f2c2cac65
marco32451-png <marco32451@gmail.com>
2025-09-19 18:09:33 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,19 +2,19 @@ import os
 from os import environ
 
 # API Configuration
-API_ID = int(os.environ.get("API_ID", "294351"))
-API_HASH = os.environ.get("API_HASH", "2d211eb63606daee")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "24962099"))
+API_HASH = os.environ.get("API_HASH", 483dda200f05eebfa9dcb5edde5467b5")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7879078729:AAFZGbStHgAiOCL0h2vi6m4Z-3WTwA8fEaE")
 
 CREDIT = os.environ.get("CREDIT", "⌯ FʀᴏɴᴛMᴀɴ | ×͜× |")
 # MongoDB Configuration
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "DevThanos")
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Dev-Thanos")  # Add your own atlas db
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "cluster0")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://marco32451_db_user:marco32451_db_user@cluster0.ye1fbhi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # Add your own atlas db
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "7793257011"))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "7793257011").split()]  # Default to owner ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "7879078729"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "7879078729").split()]  # Default to owner ID
 
 # Channel Configuration
 PREMIUM_CHANNEL = "https://t.me/+zzgp8DUgaic3MzRl"
@@ -59,6 +59,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
